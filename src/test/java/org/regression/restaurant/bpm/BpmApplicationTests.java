e7780b3fa507e8cb407841aff7179e0a003fdf71
xaxapetov <xaxapetov@yandex.ru>
2021-05-05 00:13:53 +0300

    Empty project

---
+++ src/test/java/org/regression/restaurant/bpm/BpmApplicationTests.java
@@ -0,0 +1,13 @@
+package org.regression.restaurant.bpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BpmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
